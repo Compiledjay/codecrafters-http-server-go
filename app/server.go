f128b13694a554a2e32c2542b91a13836f43d040
Compiledjay <139014932+Compiledjay@users.noreply.github.com>
2024-05-26 18:23:49 -0700

    feat: add post requests for files
    feat: add post requests for files
    
    feat: add post requests for files
    
    feat: add post requests for files
    
    fix: spacing
    
    fix: spacing
    
    fix

--- app/server.go
+++ app/server.go
@@ -11,6 +11,8 @@ import (
 const (
 	// response type
 	Response200 = "HTTP/1.1 200 OK\r\n"
+	Response201 = "HTTP/1.1 201 Created\r\n"
+	Response400 = "HTTP/1.1 400 Bad Request\r\n"
 	Response404 = "HTTP/1.1 404 Not Found\r\n"
 	// headers
 	ContentTextPlain      = "Content-Type: text/plain\r\n"
@@ -33,7 +35,7 @@ func parseRequest(req string) (*httpRequest, error) {
 	path := reqLine[1]
 
 	postReq := lines[1:]
-	mapHeaders := make(map[string]string)
+	headers := make(map[string]string)
 	for _, header := range postReq {
 		if header == "" {
 			break
@@ -42,19 +44,19 @@ func parseRequest(req string) (*httpRequest, error) {
 		headerField := strings.SplitN(header, ":", 2)
 		name := strings.Trim(headerField[0], " ")
 		value := strings.Trim(headerField[1], " ")
-		mapHeaders[strings.ToLower(name)] = value + "\r\n"
+		headers[strings.ToLower(name)] = value + "\r\n"
 	}
 	body := lines[len(lines)-1]
 
 	return &httpRequest{
 		reqType,
 		path,
-		mapHeaders,
+		headers,
 		body,
 	}, nil
 }
 
-func createResponse(r *httpRequest) string {
+func createGetResponse(r *httpRequest) string {
 	response := ""
 	switch p := strings.Split(r.path, "/")[1]; p {
 	case "":
@@ -68,15 +70,17 @@ func createResponse(r *httpRequest) string {
 		body = strings.Trim(body, "\r\n")
 		response = fmt.Sprintf("%s%s%s%d\r\n\r\n%s", Response200, ContentTextPlain, ContentLength, len(body), body)
 	case "files":
-		if len(os.Args) < 3 {
-			response = fmt.Sprintf("%s\r\n", Response404)
+		if len(os.Args) < 3 && os.Args[1] != "--directory" {
+			fmt.Println("insufficient arguments, usage: ./server.sh --directory <directory>")
+			response = fmt.Sprintf("%s\r\n", Response400)
 			break
 		}
+
 		dir := os.Args[2]
-		splitReq := strings.Split(r.path, "/")
-		fileName := splitReq[2]
-		file, err := os.Open(dir + fileName)
+		fileName := strings.Split(r.path, "/")[2]
+		file, err := os.Open(fmt.Sprintf("%s/%s", dir, fileName))
 		if err != nil {
+			fmt.Println("error opening file in directory: ", err.Error())
 			response = fmt.Sprintf("%s\r\n", Response404)
 			break
 		}
@@ -87,8 +91,46 @@ func createResponse(r *httpRequest) string {
 		for scanner.Scan() {
 			body += scanner.Text()
 		}
+
 		response = fmt.Sprintf("%s%s%s%d\r\n\r\n%s", Response200, ContentAppOctetStream, ContentLength, len(body), body)
 	default:
+		fmt.Println("unrecognized path: ", r.path)
+		response = fmt.Sprintf("%s\r\n", Response404)
+	}
+
+	return response
+}
+
+func createPostResponse(r *httpRequest) string {
+	response := ""
+	switch p := strings.Split(r.path, "/")[1]; p {
+	case "files":
+		if len(os.Args) < 3 && os.Args[1] != "--directory" {
+			fmt.Println("insufficient arguments, usage: ./server.sh --directory <directory>")
+			response = fmt.Sprintf("%s\r\n", Response400)
+			break
+		}
+
+		dir := os.Args[2]
+		fileName := strings.Split(r.path, "/")[2]
+		file, err := os.Create(fmt.Sprintf("%s/%s", dir, fileName))
+		if err != nil {
+			fmt.Println("error creating file in directory: ", err.Error())
+			response = fmt.Sprintf("%s\r\n", Response404)
+			break
+		}
+		defer file.Close()
+
+		_, err = file.WriteString(r.body)
+		if err != nil {
+			fmt.Println("error writing to file: ", err.Error())
+			response = fmt.Sprintf("%s\r\n", Response400)
+			break
+		}
+
+		response = fmt.Sprintf("%s%s%s%d\r\n\r\n%s", Response201, ContentTextPlain, ContentLength, len(r.body), r.body)
+	default:
+		fmt.Println("unrecognized path: ", r.path)
 		response = fmt.Sprintf("%s\r\n", Response404)
 	}
 
@@ -96,22 +138,39 @@ func createResponse(r *httpRequest) string {
 }
 
 func manageConnection(conn net.Conn) {
-	buffer := make([]byte, 1024)
-	n, err := conn.Read(buffer)
-	if err != nil {
-		fmt.Println("Error reading from connection: ", err.Error())
-		os.Exit(1)
+	bufSize := 1024
+	buffer := make([]byte, bufSize)
+	data := ""
+	for {
+		n, err := conn.Read(buffer)
+		if err != nil {
+			fmt.Println("Error reading from connection: ", err.Error())
+			os.Exit(1)
+		}
+
+		data += string(buffer[:n])
+		if n < bufSize {
+			break
+		}
 	}
 
-	request, err := parseRequest(string(buffer[:n]))
+	request, err := parseRequest(data)
 	if err != nil {
 		fmt.Println("Error parsing request: ", err.Error())
 	}
 
-	response := createResponse(request)
-	fmt.Println([]byte(response))
-	fmt.Println(response)
+	var response string
+	switch request.reqType {
+	case "GET":
+		response = createGetResponse(request)
+	case "POST":
+		response = createPostResponse(request)
+	default:
+		fmt.Println("Invalid request: ", request.reqType)
+		os.Exit(1)
+	}
 
+	fmt.Println(response)
 	conn.Write([]byte(response))
 	conn.Close()
 }
